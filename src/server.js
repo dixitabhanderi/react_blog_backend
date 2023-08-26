1f6872fbab6f0ad6eecbe4581171e9a57ae53690
Dixita <dixitabhanderi101@gmail.com>
2023-08-25 23:30:42 -0700

    url parameter in express and upvoting articles

--- src/server.js
+++ src/server.js
@@ -1,14 +1,43 @@
 import express from 'express';
 
-const app = express();
+// localhost:3000/article/learn-node
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+},{
+    name: 'learn-node',
+    upvotes: 0,
+},{
+    name: 'mongodb',
+    upvotes: 0,
+}]
 
+const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    console.log(req.body);
-    res.send(`Hello ${req.body.name}!`);
+// app.post('/hello', (req, res) => {
+//     console.log(req.body);
+//     res.send(`Hello ${req.body.name}!`);
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//     console.log(req.params);
+//     const { name } = req.params;
+//     res.send(`Hello ${name}!!`);
+// })
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if(article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
 });
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
+    // http://localhost:8000/hello
 });
